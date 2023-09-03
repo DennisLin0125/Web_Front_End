a9b5794fdd4bd477ff432924ac95a8e2fb960749
Dennis <740125aa@gmail.com>
2023-09-03 10:49:49 +0800

    init

---
+++ js/data.js
@@ -0,0 +1,114 @@
+var goodData = {
+    path: [
+        {
+            title: "手機、數位、通訊",
+            url: "###"
+        }, {
+            title: "手機",
+            url: "###"
+        }, {
+            title: "Apple蘋果",
+            url: "###"
+        }, {
+            title: "iPhone 6S系",
+        }
+    ],
+    imagessrc: [
+        { b: "./images/b1.png", s: "./images/s1.png" },
+        { b: "./images/b2.png", s: "./images/s2.png" },
+        { b: "./images/b3.png", s: "./images/s3.png" },
+        { b: "./images/b1.png", s: "./images/s1.png" },
+        { b: "./images/b2.png", s: "./images/s2.png" },
+        { b: "./images/b3.png", s: "./images/s3.png" },
+        { b: "./images/b1.png", s: "./images/s1.png" },
+        { b: "./images/b2.png", s: "./images/s2.png" },
+        { b: "./images/b3.png", s: "./images/s3.png" },
+        { b: "./images/b1.png", s: "./images/s1.png" },
+        { b: "./images/b2.png", s: "./images/s2.png" },
+        { b: "./images/b3.png", s: "./images/s3.png" },
+        { b: "./images/b1.png", s: "./images/s1.png" },
+        { b: "./images/b2.png", s: "./images/s2.png" }
+    ],
+    goodsDetail: {
+        title: "Apple iPhone 6s（A1700）64G玫瑰金色 移動通信電信4G手機",
+        recommend: "推薦選擇下方[移動優惠購],手机套餐齊搞定,不用換號,每月還有花費返回",
+        price: 5299,
+        promoteSales: {
+            type: "加價購",
+            content: "滿999.00另加20.00元，或滿1999.00另加30.00元，或滿2999.00另加40.00元，即可在購物車換購熱銷商品"
+        },
+        support: "以舊換新，閒置手機回收 4G套餐超值搶 禮品購",
+        address: "廣東省 深圳市 寶安區",
+        evaluateNum: 670000,
+        crumbData: [
+            {
+                "title": "選擇顏色",
+                "data": [
+                    {
+                        type: "金色",
+                        changePrice: 0
+                    },
+                    {
+                        type: "銀色",
+                        changePrice: 40
+                    },
+                    {
+                        type: "黑色",
+                        changePrice: 90
+                    },
+                ]
+            },
+            {
+                "title": "內存容量",
+                "data": [
+                    {
+                        type: "16G",
+                        changePrice: 0
+                    },
+                    {
+                        type: "64G",
+                        changePrice: 300
+                    },
+                    {
+                        type: "128G",
+                        changePrice: 900
+                    },
+                    {
+                        type: "256G",
+                        changePrice: 1300
+                    },
+                ]
+            },
+            {
+                "title": "選擇版本",
+                "data": [
+                    {
+                        type: "公開版",
+                        changePrice: 0
+                    },
+                    {
+                        type: "移動版",
+                        changePrice: -1000
+                    }
+                ]
+            },
+            {
+                "title": "購買方式",
+                "data": [
+                    {
+                        type: "官方標配",
+                        changePrice: 0
+                    },
+                    {
+                        type: "優惠移動版",
+                        changePrice: -240
+                    },
+                    {
+                        type: "電信優惠版",
+                        changePrice: -390
+                    },
+                ]
+            }
+        ]
+    }
+}
---
+++ js/index.js
@@ -0,0 +1,505 @@
+// 當所有DOM元素和相關資源都載入完畢後執行的事件函數
+window.onload = function () {
+
+    //宣告一個紀錄點擊縮略圖的下標
+    var bigimgIndex = 0;
+
+    // 路徑導航的數據綁定
+    navPathDataBind();
+    function navPathDataBind() {
+        /**
+         * 思路：
+         * 1、首先獲取路徑導航的頁面元素（navPath）
+         * 2、然後獲取所需的數據（data.js->goodData.path）
+         * 3、由於數據是需要動態生成的，因此相應的DOM元素也應該是動態生成的，含義需要根據數據的數量來創建DOM元素
+         * 4、在遍歷數據並創建DOM元素時，當處理最後一個數據時，只創建a標籤，而不創建i標籤
+         */
+
+        // 1. 獲取頁面導航的元素對象
+        var navPath = document.querySelector('#wrapper #content .contentMain #navPath');
+
+        // 2. 獲取數據
+        var path = goodData.path;
+
+        // 3. 遍歷數據
+        for (var i = 0; i < path.length; i++) {
+            if (i == path.length - 1) {
+                // 只需創建a標籤並且不添加href屬性
+                var aNode = document.createElement("a");
+                aNode.innerText = path[i].title;
+                navPath.appendChild(aNode);
+            } else {
+                // 4. 創建a標籤
+                var aNode = document.createElement("a");
+                aNode.href = path[i].url;
+                aNode.innerText = path[i].title;
+
+                // 5. 創建i標籤
+                var iNode = document.createElement('i');
+                iNode.innerText = '/';
+
+                // 6. 讓navPath元素添加a和i
+                navPath.appendChild(aNode);
+                navPath.appendChild(iNode);
+            }
+        }
+    }
+
+    // 放大鏡的移入、移出效果
+    bigClassBind();
+    function bigClassBind(){
+        /**
+         * 思路：
+         * 1、獲取小圖框元素對象，並設置移入事件（onmouseenter）
+         * 2、動態創建蒙版元素、大圖框和大圖片元素
+         * 3、移出時（onmouseleave）需要移除蒙版元素和大圖框
+         */
+
+        // 1. 獲取小圖框元素
+        var smallPic = document.querySelector('#wrapper #content .contentMain #center #left #leftTop #smallPic');
+        // 獲取leftTop元素
+        var leftTop = document.querySelector('#wrapper #content .contentMain #center #left #leftTop');
+        
+        // 獲取數據
+        var imagessrc = goodData.imagessrc
+        
+        // 2. 設置移入事件
+        smallPic.onmouseenter = function(){
+
+            // 3. 創建蒙版元素
+            var maskDiv = document.createElement('div');
+            maskDiv.className = "mask";
+
+            // 4. 創建大圖框元素
+            var BigPic = document.createElement('div');
+            BigPic.id = "bigPic";
+
+            // 5. 創建大圖片元素
+            var BigImg = document.createElement('img');
+            BigImg.src = imagessrc[bigimgIndex].b;
+
+            // 6. 大圖框添加大圖片
+            BigPic.appendChild(BigImg);
+
+            // 7. 讓小圖框添加蒙版元素
+            smallPic.appendChild(maskDiv);
+
+            // 8. 讓leftTop元素添加大圖框
+            leftTop.appendChild(BigPic);
+
+            // 設置移動事件
+            smallPic.onmousemove = function(event){
+                // event.clientX: 鼠標點距離瀏覽器左側X軸的值
+                // getBoundingClientRect().left:小圖框元素距離瀏覽器左側可視區域left值
+                // offsetWidth: 元素的占位寬度
+                var left = event.clientX - smallPic.getBoundingClientRect().left - maskDiv.offsetWidth / 2;
+                var top = event.clientY - smallPic.getBoundingClientRect().top - maskDiv.offsetHeight / 2;
+
+                // 判斷邊界
+                if(left < 0){
+                    left = 0;
+                } else if(left > smallPic.clientWidth - maskDiv.offsetWidth){
+                    left = smallPic.clientWidth - maskDiv.offsetWidth;
+                }
+
+                if(top < 0){
+                    top = 0;
+                } else if(top > smallPic.clientHeight - maskDiv.offsetHeight){
+                    top = smallPic.clientHeight - maskDiv.offsetHeight;
+                }
+
+                // 設置left和top屬性
+                maskDiv.style.left = left + "px";
+                maskDiv.style.top = top + "px";
+
+                // 移動的比例關係 = 蒙版元素移動的距離 / 大圖片元素移動的距離
+                // 蒙版元素移動的距離 = 小圖框寬度 – 蒙版元素的寬度
+                // 大圖片元素移動的距離 = 大圖片寬度 – 大圖框元素的寬度
+                var scale = (smallPic.clientWidth - maskDiv.offsetWidth) / (BigImg.offsetWidth - BigPic.clientWidth);
+
+                BigImg.style.left = -left / scale + 'px';
+                BigImg.style.top = -top / scale + 'px';
+            }
+
+            // 設置移出事件
+            smallPic.onmouseleave = function(){
+
+                // 移除小圖框的蒙版元素
+                smallPic.removeChild(maskDiv);
+
+                // 移除leftTop元素的大圖框
+                leftTop.removeChild(BigPic);
+            }
+        }
+    }
+
+    // 動態渲染放大鏡縮略圖的數據
+    thunbnaiData();
+    function thunbnaiData(){
+        /**
+         * 思路：
+         * 1、先獲取piclist元素下的ul
+         * 2、再獲取data.js文件下的goodData->imagessrc
+         * 3、遍歷數組，根據數組的長度來創建li元素
+         * 4、讓ul遍歷追加li元素
+         */
+
+        //1.獲取piclist下的ul
+        var ul = document.querySelector('#wrapper #content .contentMain #center #left #leftBottom #piclist ul');
+                
+        //2.獲取imagessrc數據
+        var imagessrc = goodData.imagessrc;
+                
+        //3.遍歷數組
+        for(var i = 0; i< imagessrc.length;i++){
+            //4.創建li元素
+            var newLi = document.createElement('li');
+                    
+            //5.創建img元素
+            var newImg = document.createElement('img');
+            newImg.src = imagessrc[i].s;
+
+            //6.讓li追加img元素
+            newLi.appendChild(newImg);
+
+            //7.讓ul追加li元素
+            ul.appendChild(newLi);
+        }
+
+    }
+
+    // 縮略圖點擊效果
+    thumbnailClick();
+    function thumbnailClick() {
+        /**
+         * 思路：
+         * 1、獲取所有的li元素，並循環設置點擊事件
+         * 2、點擊縮略圖需要確定其下標位置，以找到相應的小圖路徑和大圖路徑，替換現有src的值
+         */
+
+        // 1.獲取所有的li元素
+        var liNodes = document.querySelectorAll('#wrapper #content .contentMain #center #left #leftBottom #piclist ul li img');
+
+        var smallPic_img = document.querySelector('#wrapper #content .contentMain #center #left #leftTop #smallPic img');
+
+        var imagessrc = goodData.imagessrc;
+
+        // 小圖路徑需要默認與imagessrc的第一個元素的小圖路徑相同
+        smallPic_img.src = imagessrc[0].s;
+
+        // 2.循環點擊這些li元素
+        for (var i = 0; i < liNodes.length; i++) {
+            // 在點擊事件之前，為每個元素添加自定義的下標
+            liNodes[i].index = i; /** 還可以通過setAttribute('index',i) */
+            liNodes[i].onclick = function () {
+                var idx = this.index; /** 事件函數中的this永遠指向實際發生事件的目標源對象 */
+                bigimgIndex = idx;
+
+                // 變換小圖路徑
+                smallPic_img.src = imagessrc[idx].s;
+            }
+        }
+    }
+
+    // 點擊縮略圖左右箭頭的效果
+    thumbnailLeftRightClick();
+    function thumbnailLeftRightClick(){
+        /**
+         * 思路：
+         * 1、先獲取左右兩端的箭頭按鈕
+         * 2、在獲取可視的div以及ul元素和所有的li元素
+         * 3、計算（發生起點、步長、總體運動的距離值）
+         * 4、然後再發生點擊事件
+         */
+
+        //1、獲取箭頭元素
+        var prev = document.querySelector('#wrapper #content .contentMain #center #left #leftBottom a.prev');
+        var next = document.querySelector('#wrapper #content .contentMain #center #left #leftBottom a.next');
+
+        
+        //2、獲取可視的div以及ul元素和所有的li元素
+        var ul = document.querySelector('#wrapper #content .contentMain #center #left #leftBottom #piclist ul');
+        
+        var liNodes = document.querySelectorAll('#wrapper #content .contentMain #center #left #leftBottom #piclist ul li');
+
+        //3、計算
+
+        //發生起點
+        var start = 0;
+
+        //步長
+        var step = (liNodes[0].offsetWidth + 20) * 2;
+
+        //總體運動的距離值 = ul的寬度 - div框的寬度 = (圖片的總數 - div中顯示的數量) * （li的寬度 + 20）
+        var endPostion = (liNodes.length - 5 ) * (liNodes[0].offsetWidth + 20);
+
+        //4、發生事件
+        prev.onclick = function(){
+            start-=step;
+            console.log(start);
+            if(start < 0){
+                start = 0;
+            }
+
+            ul.style.left = -start + "px";
+        }
+
+        next.onclick = function(){
+            start+=step;
+            if(start > endPostion){
+                start = endPostion;
+            }
+
+            ul.style.left = -start + "px";
+        }
+    }
+
+    // 商品詳細資料的動態渲染
+    rightTopData();
+    function rightTopData() {
+        /**
+         * 思路：
+         * 1、查找rightTop元素
+         * 2、查找data.js->goodData->goodsDetail
+         * 3、建立一個字串變數，將原本的佈局結構貼上，將所對應的數據放在對應的位置上重新渲染rightTop元素
+         */
+
+        //1、查找元素
+        var rightTop = document.querySelector('#wrapper #content .contentMain #center #right .rightTop');
+
+        //2、查找數據
+        var goodsDetail = goodData.goodsDetail;
+
+        //3、建立一個字串（雙引號、單引號、模板字串）變數
+        //模板字串替換數據：${變數}
+        var s = `<h3>${goodsDetail.title}</h3>
+                <p>${goodsDetail.recommend}</p>
+                <div class="priceWrap">
+                    <div class="priceTop">
+                        <span>價&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;格</span>
+                        <div class="price">
+                            <span>￥</span>
+                            <p>${goodsDetail.price}</p>
+                            <i>降價通知</i>
+                        </div>
+                        <p>
+                            <span>累計評價</span>
+                            <span>${goodsDetail.evaluateNum}</span>
+                        </p>
+                    </div>
+                    <div class="priceBottom">
+                        <span>促&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;銷</span>
+                        <p>
+                            <span>${goodsDetail.promoteSales.type}</span>
+                            <span>${goodsDetail.promoteSales.content}</span>
+                        </p>
+                    </div>
+                </div>
+                <div class="support">
+                    <span>支&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;持</span>
+                    <p>${goodsDetail.support}</p>
+                </div>
+                <div class="address">
+                    <span>配&nbsp;送&nbsp;至</span>
+                    <p>${goodsDetail.address}</p>
+                </div>`;
+
+        //4、重新渲染rightTop元素
+        rightTop.innerHTML = s;
+    }
+
+    // 商品參數資料的動態渲染
+    rightBottomData();
+    function rightBottomData() {
+        /**
+         * 思路：
+         * 1、找rightBottom的元素對象
+         * 2、查找data.js->goodData.goodsDetail.crumbData資料
+         * 3、由於資料是一個陣列，需要遍歷，有一個元素則需要有一個動態的dl元素對象(dt、dd)
+         */
+
+        //1、查找元素对象
+        var chooseWrap = document.querySelector('#wrapper #content .contentMain #center #right .rightBottom .chooseWrap');
+
+        //2、查找資料
+        var crumbData = goodData.goodsDetail.crumbData;
+        
+        //3、循環資料
+        for(var i = 0; i< crumbData.length;i++){
+
+            //4、創建dl元素對象
+            var dlNode = document.createElement('dl');
+
+            //5、創建dt元素對象
+            var dtNode = document.createElement('dt');
+            dtNode.innerText = crumbData[i].title;
+
+            //6、dl追加dt
+            dlNode.appendChild(dtNode);
+
+            //7、遍歷crumbData->data元素
+            for(var j = 0;j< crumbData[i].data.length;j++){
+                
+                //創建dd元素
+                var ddNode = document.createElement('dd');
+                ddNode.innerText = crumbData[i].data[j].type;
+                ddNode.setAttribute('price',crumbData[i].data[j].changePrice);
+
+                //讓dl來追加dd
+                dlNode.appendChild(ddNode);
+            }
+
+            //8、讓chooseWrap追加dl
+            chooseWrap.appendChild(dlNode);
+        }
+    }
+
+    // 點擊商品參數後的顏色排他效果
+    clickddBind();
+    function clickddBind(){
+        /**
+         * 每一行dd顏色排他的思路：
+         * 1、獲取所有的dl元素，取其中第一個dl元素下的所有dd先做測試
+         *    測試完成之後在對應dl第一行下標的前面再嵌套一個for循環，目的在變換下標
+         * 2、循環所有的dd元素並添加點擊事件
+         * 3、確定實際發生事件的目標源對象設置其文字顏色為紅色，然後給其他所有元素的顏色都重置為基礎顏色(#666)
+         * ============================================================================================
+         * 
+         * 
+         * 
+         * 點擊dd之後產生mark標籤的思路
+         * 1. 首先先創建一個可以容納點擊的dd元素值得容器(陣列),確定陣列的起始長度
+         * 2. 然後再將點擊的dd元素的值按照對應下標來寫入到陣列的元素內
+         */
+
+        //1、找第一個dl下的所有dd元素              
+        let dlNodes = document.querySelectorAll('#wrapper #content .contentMain #center #right .rightBottom .chooseWrap dl');
+        
+        let arr = new Array(dlNodes.length);
+
+        var choose = document.querySelector('#wrapper #content .contentMain #center #right .rightBottom .choose');
+        
+        arr.fill(0); /* [0,0,0,0] */
+
+        for (let i = 0; i < dlNodes.length; i++){
+
+            // 使用 let 來聲明 ddNodes，使它成為塊級作用域變量
+            let ddNodes = dlNodes[i].querySelectorAll('dd');
+
+            //2、遍歷當前所有的dd元素
+            for(var j = 0;j<ddNodes.length;j++){
+
+                ddNodes[j].onclick = function(){
+
+                    // 清空choose元素
+                    choose.innerHTML = "";
+
+                    for(let k = 0;k<ddNodes.length;k++){
+                        ddNodes[k].style.color = "#666";
+                    }
+                    
+                    //假設點擊的是第二個元素，下標為1'
+                    //ddNodes[1].style.color = "red";
+                    //相同下標的dd元素的字體顏色在進行覆蓋操作，而其他未點擊的元素都是在進行重新設置顏色
+                    this.style.color = "red";
+
+                    // 點擊的訊息存入arr
+                    arr[i] = this;
+
+                    // 呼叫函數
+                    changePriceBind(arr);
+                    
+                    // 遍歷陣列
+                    arr.forEach(function(value,index){
+                        // 如果不為0
+                        if (value) {
+                            // 創建Div標籤
+                            let markDiv = document.createElement("div");
+                            // 設置class屬性
+                            markDiv.className = "mark";
+                            // 設置值
+                            markDiv.innerText = value.innerText;
+                            // 創建 a 標籤
+                            let aNode = document.createElement("a");
+                            // 並且設置值
+                            aNode.innerText = "X";
+                            // 並且設置下標
+                            aNode.setAttribute("index",index);
+                            // 讓div追加a
+                            markDiv.appendChild(aNode);
+                            // 讓choose元素追加div
+                            choose.appendChild(markDiv);
+                        }
+                    });
+
+                    // 獲取所有a標籤,並且循環發生點擊事件
+                    let aNodes = document.querySelectorAll("#wrapper #content .contentMain #center #right .rightBottom .choose .mark a");
+                    
+                    console.log(aNodes);
+
+                    for (let n = 0; n < aNodes.length; n++) {
+                        aNodes[n].onclick = function(){ 
+                            // 獲取點擊的a標籤的index屬性值
+                            let idx1 = this.getAttribute('index');
+
+                            // 恢復陣列中對應下標元素
+                            arr[idx1] = 0;
+
+                            // 找到對應下標的那個dl行中的dd元素
+                            let ddlist = dlNodes[idx1].querySelectorAll('dd');
+
+                            for(let m = 0; m < ddlist.length;m++){
+                                //其余所有dd的文字颜色为灰色
+                                ddlist[m].style.color = "#666";
+                            }
+
+                            // 默認選中第一個顏色為紅色
+                            ddlist[0].style.color = 'red';
+
+                            // 刪除對應下標的mark元素
+                            choose.removeChild(this.parentNode);
+
+                            // 調用價格函數
+                            changePriceBind(arr);
+                        };
+                    }
+                }
+            }
+        }
+    }
+
+    // 價格變動函數聲明
+    // 函式宣告用於價格變動
+    /**
+     * 此函式應在點擊 dd 元素和刪除 mark 標記時調用。
+     */
+    function changePriceBind(arr) {
+        /**
+         * 思路：
+         * 1、獲取價格的標籤元素。
+         * 2、為每個 dd 標籤設置一個自定義屬性，用於記錄價格變動。
+         * 3、遍歷 arr 陣列，將 dd 元素上的新價格與默認價格（5299）相加。
+         * 4、最後將計算後的結果更新到 p 標籤中。
+         */
+    
+        // 1、原始價格標籤元素                   
+        var oldPrice = document.querySelector('#wrapper #content .contentMain #center #right .rightTop .priceWrap .priceTop .price p');
+        
+        // 提取默認價格
+        var price = goodData.goodsDetail.price;
+    
+        // 2、遍歷 arr 陣列
+        for (var i = 0; i < arr.length; i++) {
+
+            if (arr[i]) {
+                // 強制資料類型轉換
+                var changeprice = Number(arr[i].getAttribute('price'));
+                // 最終價格
+                price += changeprice;
+            }
+        }
+    
+        oldPrice.innerText = price;
+    }
+  
+}
